070f1e452d564be07d8b9ca36ea935c489f6450c
Rick Lambrechts <rick@rl-webdiensten.nl>
2022-12-19 14:31:12 +0100

    Base64 encodes certificates in result providers config file (#13)

--- app/Data/ResultProviderCertificate.php
+++ app/Data/ResultProviderCertificate.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\Data;
 
+use RuntimeException;
+
 class ResultProviderCertificate
 {
+    /**
+     * @psalm-pure
+     * @param string $cert
+     * @param string $chain
+     */
     public function __construct(
         protected readonly string $cert,
         protected readonly string $chain = "",
@@ -25,5 +32,28 @@ class ResultProviderCertificate
     public function hasChain(): bool
     {
         return !empty($this->chain);
+    }
+
+    /**
+     * @psalm-pure
+     * @param string $cert
+     * @param string $chain
+     * @return self
+     */
+    public static function fromBase64CertAndChain(string $cert, string $chain = ""): self
+    {
+        $cert = base64_decode($cert, true);
+        if (!empty($chain)) {
+            $chain = base64_decode($chain, true);
+        }
+
+        if (!is_string($cert) || !is_string($chain)) {
+            throw new RuntimeException('Invalid base64 encoded certificate or chain');
+        }
+
+        return new self(
+            cert: $cert,
+            chain: $chain,
+        );
     }
 }

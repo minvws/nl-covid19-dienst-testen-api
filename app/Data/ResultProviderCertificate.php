e79bea5953005d7c12d86829b710dd28640c99da
Rick Lambrechts <rick@rl-webdiensten.nl>
2022-12-19 11:08:15 +0100

    Annotate with pure

--- app/Data/ResultProviderCertificate.php
+++ app/Data/ResultProviderCertificate.php
@@ -8,6 +8,11 @@ use RuntimeException;
 
 class ResultProviderCertificate
 {
+    /**
+     * @psalm-pure
+     * @param string $cert
+     * @param string $chain
+     */
     public function __construct(
         protected readonly string $cert,
         protected readonly string $chain = "",
@@ -29,10 +34,18 @@ class ResultProviderCertificate
         return !empty($this->chain);
     }
 
+    /**
+     * @psalm-pure
+     * @param string $cert
+     * @param string $chain
+     * @return self
+     */
     public static function fromBase64CertAndChain(string $cert, string $chain = ""): self
     {
         $cert = base64_decode($cert, true);
-        $chain = base64_decode($chain, true);
+        if (!empty($chain)) {
+            $chain = base64_decode($chain, true);
+        }
 
         if (!is_string($cert) || !is_string($chain)) {
             throw new RuntimeException('Invalid base64 encoded certificate or chain');
